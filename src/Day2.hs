99a76ffdc8e6c58eb63d2200cec762c885a452d0
Marc Reisner <mreisner@epic.com>
2021-12-02 09:33:36 -0600

    Reduce code duplication

--- src/Day2.hs
+++ src/Day2.hs
@@ -5,10 +5,13 @@ data Command = Forward Int | Down Int | Up Int
 data Location = Location {depth :: Int, horizontal :: Int, aim :: Int}
 
 part1 :: [String] -> String
-part1 = show . multiplyLocation . foldl doCommandNoAim Location {depth = 0, horizontal = 0, aim = 0} . parseCommands
+part1 = executeCommands doCommandWithoutAim
 
 part2 :: [String] -> String
-part2 = show . multiplyLocation . foldl doCommandWithAim Location {depth = 0, horizontal = 0, aim = 0} . parseCommands
+part2 = executeCommands doCommandWithAim
+
+executeCommands :: (Location -> Command -> Location) -> [String] -> String
+executeCommands doCommand = show . multiplyLocation . foldl doCommand Location {depth = 0, horizontal = 0, aim = 0} . parseCommands
 
 parseCommands :: [String] -> [Command]
 parseCommands = reverse . foldl buildCommand [] . map words
@@ -18,10 +21,10 @@ parseCommands = reverse . foldl buildCommand [] . map words
     buildCommand acc ("up" : n : _) = Up (read n) : acc
     buildCommand _ _ = undefined
 
-doCommandNoAim :: Location -> Command -> Location
-doCommandNoAim loc (Forward x) = loc {horizontal = horizontal loc + x}
-doCommandNoAim loc (Down x) = loc {depth = depth loc + x}
-doCommandNoAim loc (Up x) = loc {depth = depth loc - x}
+doCommandWithoutAim :: Location -> Command -> Location
+doCommandWithoutAim loc (Forward x) = loc {horizontal = horizontal loc + x}
+doCommandWithoutAim loc (Down x) = loc {depth = depth loc + x}
+doCommandWithoutAim loc (Up x) = loc {depth = depth loc - x}
 
 doCommandWithAim :: Location -> Command -> Location
 doCommandWithAim loc (Forward x) = loc {horizontal = horizontal loc + x, depth = depth loc + aim loc * x}
@@ -29,4 +32,4 @@ doCommandWithAim loc (Down x) = loc {aim = aim loc + x}
 doCommandWithAim loc (Up x) = loc {aim = aim loc - x}
 
 multiplyLocation :: Location -> Int
-multiplyLocation (Location y x _) = y * x
+multiplyLocation (Location d h _) = d * h
