0b29751faeb9801c594ebfd8758e5c8667c461fa
Marc Reisner <mreisner@epic.com>
2021-12-03 12:00:38 -0600

    Complete day 3 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -64,6 +64,7 @@ solve 1 2 xs = MaybeT $ return $ Just $ Day1.part2 $ lines xs
 solve 2 1 xs = MaybeT $ return $ Just $ Day2.part1 $ lines xs
 solve 2 2 xs = MaybeT $ return $ Just $ Day2.part2 $ lines xs
 solve 3 1 xs = MaybeT $ return $ Just $ Day3.part1 $ lines xs
+solve 3 2 xs = MaybeT $ return $ Just $ Day3.part2 $ lines xs
 solve day part _ = do
   lift $ hPutStrLn stderr $ "Day " ++ show day ++ " Part " ++ show part ++ " not yet implemented."
   MaybeT $ return Nothing
--- src/Day3.hs
+++ src/Day3.hs
@@ -1,17 +1,51 @@
-module Day3 (part1) where
+module Day3 (part1, part2) where
 
 import Control.Applicative
 import Data.Bits
 import Data.Char
 
+type RateFunction = [String] -> Int
+
+type BitCriteria = [String] -> String
+
 part1 :: [String] -> String
 part1 input = show $ getEpsilonRate input * getGammaRate input
 
-getGammaRate :: [String] -> Int
-getGammaRate = binaryToInt . getBinary . avgColumns
+part2 :: [String] -> String
+part2 input = show $ getOxygenGeneratorRating input * getCarbonDioxideScrubberRating input
 
-getEpsilonRate :: [String] -> Int
-getEpsilonRate = binaryToInt . getInvertedBinary . avgColumns
+getRating' :: Int -> BitCriteria -> [String] -> String
+getRating' _ _ [x] = x
+getRating' n criteriaFunc xs = getRating' (n + 1) criteriaFunc $ filterReport criteriaFunc n xs
+
+getRating :: BitCriteria -> [String] -> String
+getRating = getRating' 0
+
+getOxygenGeneratorRating :: [String] -> Int
+getOxygenGeneratorRating = binaryToInt . getRating oxygenGeneratorBitCriteria
+
+getCarbonDioxideScrubberRating :: [String] -> Int
+getCarbonDioxideScrubberRating = binaryToInt . getRating carbonDioxideScrubberBitCriteria
+
+filterReport :: BitCriteria -> Int -> [String] -> [String]
+filterReport criteriaFunc n xs = filter (\x -> criteria !! n == x !! n) xs
+  where
+    criteria = criteriaFunc xs
+
+oxygenGeneratorBitCriteria :: [String] -> String
+oxygenGeneratorBitCriteria = bitCriteria (\x -> if x >= 0.5 then '1' else '0')
+
+carbonDioxideScrubberBitCriteria :: [String] -> String
+carbonDioxideScrubberBitCriteria = bitCriteria (\x -> if x >= 0.5 then '0' else '1')
+
+bitCriteria :: (Float -> Char) -> [String] -> String
+bitCriteria criteriaFunc = map criteriaFunc . avgColumns
+
+getGammaRate :: RateFunction
+getGammaRate = binaryToInt . oxygenGeneratorBitCriteria
+
+getEpsilonRate :: RateFunction
+getEpsilonRate = binaryToInt . carbonDioxideScrubberBitCriteria
 
 intToFloat :: (Integral a) => a -> Float
 intToFloat = fromIntegral
